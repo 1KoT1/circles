4b229f490fea58ab9849e4e6d8ac8582d69d92d1
Василий Почкаенко <pochkaenkov@yandex.ru>
2015-03-21 01:11:28 +0700

    Добился правильной отрисовки взрывов.

--- circles/fireControl.js
+++ circles/fireControl.js
@@ -2,22 +2,26 @@ var component;
 var fires = [];
 
 function updateCircles(circles) {
+    if(component === undefined) {
+        console.debug("aaa");
+        component = Qt.createComponent("Fire.qml");
+    }
     if(circles.length > fires.length) {
         for(var i = fires.length; i < circles.length; i++) {
-            createFire(circles[i])
+            createFire(circles[i]);
         }
     } else {
-        for(var i = circles.length; i < fires.length; i++) {
-            fires[i].destroy();
+        while(fires.length > circles.length) {
+            var fire = fires.shift();
+            fire.destroy();
         }
     }
 }
 
 function createFire(circle) {
-    component = Qt.createComponent("Fire.qml");
-    fires[circle.center] = component.createObject(gameDiplay, {"x": circle.center.x, "y": circle.center.y, "r": circle.radius});
+    fires.push(component.createObject(gameDiplay, {"x": circle.center.x, "y": circle.center.y, "r": circle.radius}));
 
-    if (fires[circle.center] === null) {
+    if (fires[fires.length - 1] === null) {
         console.error(qsTr("Ошибка создания объекта"));
     }
 }
